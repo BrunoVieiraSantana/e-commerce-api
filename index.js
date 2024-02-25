4339c4562f4018dbff3fa82867dbaa4e7a1727cf
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-02-25 14:52:02 -0300

    commit 52

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 
+require('dotenv').config()
+
 app.use(express.json())
 
 const productRouter = require('./routes/product.router')
 
 app.use("/api/v1/products", productRouter)
 
-app.listen(5000, () => console.log("Server is running on port 5000"))
+app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
