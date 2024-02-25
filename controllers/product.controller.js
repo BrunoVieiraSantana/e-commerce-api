1caf8f6990e5813bfb962adbca363f23d616f301
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-02-25 14:34:03 -0300

    commit 50

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,13 +2,70 @@ const postgre = require('../database')
 const productController = {
     getAll: async(req, res) => {
         try {
-            res.json({msg: "OK"})
+            const { rows } = await postgre.query("select * from products")
+            res.json({msg: "OK", data: rows})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },
+    getById: async(req, res) => {
+        try {
+            const { rows } = await postgre.query("select * from products where id_product = $1", [req.params.id])
+
+            if (rows[0]) {
+                return res.json({msg: "OK", data: rows})
+            }
+
+            res.status(404).json({msg: "not found"})
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },
+    create: async(req, res) => {
+        try {
+            const { title, descript, currentPrice, mainimg, thumbnail } = req.body
+
+            const sql = 'INSERT INTO products(title, descript, currentPrice, mainimg, thumbnail) VALUES($1, $2, $3, $4, $5) RETURNING *';
+
+            const { rows } = await postgre.query(sql, [title, descript, currentPrice, mainimg, thumbnail]);
+
+            res.json({msg: "OK", data: rows[0]})
+
+        } catch (error) {
+            res.json({msg: error.msg})
+        }
+    },
+    updateById: async (req, res) => {
+        try {
+            const { title, descript, currentPrice, mainimg, thumbnail } = req.body;
+    
+            const sql = 'UPDATE products SET title = $1, descript = $2, currentPrice = $3, mainimg = $4, thumbnail = $5 WHERE id_product = $6 RETURNING *';
+    
+            const { rows } = await postgre.query(sql, [title, descript, currentPrice, mainimg, thumbnail, req.params.id]);
+    
+            res.json({ msg: "OK", data: rows[0] });
+        } catch (error) {
+            res.json({ msg: error.msg });
+        }
+    },
+    
+    deleteById: async(req, res) => {
+        try {
+            const sql = 'DELETE FROM products where id_product = $1 RETURNING *'
+
+            const { rows } = await postgre.query(sql, [req.params.id])
+
+            if (rows[0]) {
+                return res.json({msg: "OK", data: rows[0]})
+            }
+
+            return res.status(404).json({msg: "not found"})
+            
+
         } catch (error) {
             res.json({msg: error.msg})
         }
     }
-
 }
-
 
 module.exports = productController
--- routes/product.router.js
+++ routes/product.router.js
@@ -4,5 +4,9 @@ const router = express.Router()
 const productController = require('../controllers/product.controller')
 
 router.get("/", productController.getAll)
+router.get("/:id", productController.getById)
+router.post("/", productController.create)
+router.put("/:id", productController.updateById)
+router.delete("/:id", productController.deleteById)
 
 module.exports = router
