2c59941531903ad9876675d1e725bcbc987a53a4
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-04 17:17:54 -0300

    commit work 17

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -1,58 +1,76 @@
 const postgre = require('../database');
 
-const createPurchase = async (req, res) => {
-  try {
-    const { user_id } = req.body;
+const purchaseController = {
+    createPurchase: async (req, res) => {
+        try {
+            const { user_id, ...item } = req.body;
+            console.log(`User ID: ${user_id}, Item:`, item);
 
-    for (const item of req.body) {
-      const { product_id, purchase_price, quantity, status } = item;
+            const { product_id, purchase_price, quantity, status } = item;
 
-      const { rows: productRows } = await postgre.query("SELECT stock FROM products WHERE id_product = $1", [product_id]);
-      
-      const availableStock = productRows[0].stock;
+            const { rows: productRows } = await postgre.query("SELECT stock FROM products WHERE id_product = $1", [product_id]);
+            
+            const availableStock = productRows[0].stock;
 
-      if (quantity > availableStock) {
-        return res.status(400).json({ msg: "Not enough stock available" });
-      }
+            if (quantity > availableStock) {
+                return res.status(400).json({ msg: "Not enough stock available" });
+            }
 
-      await postgre.query('BEGIN');
+            await postgre.query('BEGIN');
 
-      const purchaseDate = new Date().toISOString();
-      const purchaseQuery = `
-          INSERT INTO purchases (purchase_date)
-          VALUES ($1)
-          RETURNING id_purchase
-      `;
-      const purchaseValues = [purchaseDate];
-      const { rows: purchaseRows } = await postgre.query(purchaseQuery, purchaseValues);
-      const purchaseId = purchaseRows[0].id_purchase;
+            const purchaseDate = new Date().toISOString();
+            const purchaseQuery = `
+                INSERT INTO purchases (purchase_date)
+                VALUES ($1)
+                RETURNING id_purchase
+            `;
+            const purchaseValues = [purchaseDate];
+            const { rows: purchaseRows } = await postgre.query(purchaseQuery, purchaseValues);
+            const purchaseId = purchaseRows[0].id_purchase;
 
-      const itemQuery = `
-          INSERT INTO items (user_id, product_id, purchase_id, purchase_price, quantity, status)
-          VALUES ($1, $2, $3, $4, $5, $6)
-          RETURNING *
-      `;
-      const itemValues = [user_id, product_id, purchaseId, purchase_price, quantity, status];
-      const { rows: itemRows } = await postgre.query(itemQuery, itemValues);
+            const itemQuery = `
+                INSERT INTO items (user_id, product_id, purchase_id, purchase_price, quantity, status)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING *
+            `;
+            const itemValues = [user_id, product_id, purchaseId, purchase_price, quantity, status];
+            const { rows: itemRows } = await postgre.query(itemQuery, itemValues);
 
-      const updateStockQuery = `
-          UPDATE products 
-          SET stock = stock - $1 
-          WHERE id_product = $2;
-      `;
-      await postgre.query(updateStockQuery, [quantity, product_id]);
+            const updateStockQuery = `
+                UPDATE products 
+                SET stock = stock - $1 
+                WHERE id_product = $2;
+            `;
+            await postgre.query(updateStockQuery, [quantity, product_id]);
 
-      await postgre.query('COMMIT');
+            await postgre.query('COMMIT');
+
+            res.json({ msg: "Purchase completed successfully", data: itemRows[0] });
+
+        } catch (error) {
+            await postgre.query('ROLLBACK');
+            console.error(error);
+            res.status(500).json({ msg: "Internal Server Error" });
+        }
+    },
+
+    getAllPurchasesByUser: async (req, res) => {
+        try {
+            const { user_id } = req.params;
+
+            const query = `
+                SELECT * FROM items
+                WHERE user_id = $1;
+            `;
+
+            const { rows: purchases } = await postgre.query(query, [user_id]);
+
+            res.json({ purchases });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: "Internal Server Error" });
+        }
     }
-
-    res.json({ msg: "Purchase completed successfully" });
-
-  } catch (error) {
-    await postgre.query('ROLLBACK');
-    console.error(error);
-    res.status(500).json({ msg: "Internal Server Error" });
-  }
 };
 
-module.exports = { createPurchase };
-
+module.exports = purchaseController;
