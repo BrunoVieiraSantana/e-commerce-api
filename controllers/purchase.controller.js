f5714bd03209e09f3d6c1268b0f010ebc5a4533b
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-03-04 12:45:18 -0300

    commit home 31

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -3,70 +3,66 @@ const postgre = require('../database');
 const purchaseController = {
     createPurchase: async (req, res) => {
         try {
-            const { user_id, product_id, purchase_price, quantity, status } = req.body;
-
-            // Consulta para obter o estoque disponível do produto
-            const query = "SELECT stock FROM products WHERE id_product = $1";
-            const { rows: productRows } = await postgre.query(query, [product_id]);
-
-            // Verificar se há resultados da consulta
-            if (productRows.length === 0) {
-                return res.status(404).json({ msg: "Product not found" });
-            }
-
-            // Obter o estoque disponível
-            const availableStock = productRows[0].stock;
-
-            if (quantity > availableStock) {
-                return res.status(400).json({ msg: "Not enough stock available" });
-            }
+            const { user_id, cartItems } = req.body;
 
             await postgre.query('BEGIN');
 
-            const purchaseDate = new Date().toISOString();
-            const purchaseQuery = `
-                INSERT INTO purchases (purchase_date)
-                VALUES ($1)
-                RETURNING id_purchase
-            `;
-            const purchaseValues = [purchaseDate];
-            const { rows: purchaseRows } = await postgre.query(purchaseQuery, purchaseValues);
-            const purchaseId = purchaseRows[0].id_purchase;
+            for (const item of cartItems) {
+                const { product_id, quantity } = item;
 
-            const itemQuery = `
-                INSERT INTO items (user_id, product_id, purchase_id, purchase_price, quantity, status)
-                VALUES ($1, $2, $3, $4, $5, $6)
-                RETURNING *
-            `;
-            const itemValues = [user_id, product_id, purchaseId, purchase_price, quantity, status];
-            const { rows: itemRows } = await postgre.query(itemQuery, itemValues);
+                // Consulta para obter o estoque disponível do produto
+                const query = "SELECT stock FROM products WHERE id_product = $1";
+                const { rows: productRows } = await postgre.query(query, [product_id]);
 
-            const updateStockQuery = `
-                UPDATE products 
-                SET stock = stock - $1 
-                WHERE id_product = $2;
-            `;
-            await postgre.query(updateStockQuery, [quantity, product_id]);
+                // Verificar se há resultados da consulta
+                if (productRows.length === 0) {
+                    await postgre.query('ROLLBACK');
+                    return res.status(404).json({ msg: `Product with ID ${product_id} not found` });
+                }
+
+                // Obter o estoque disponível
+                const availableStock = productRows[0].stock;
+
+                if (quantity > availableStock) {
+                    await postgre.query('ROLLBACK');
+                    return res.status(400).json({ msg: `Not enough stock available for product with ID ${product_id}` });
+                }
+
+                // Inserção da compra
+                const purchaseDate = new Date().toISOString();
+                const purchaseQuery = `
+                    INSERT INTO purchases (purchase_date, user_id)
+                    VALUES ($1, $2)
+                    RETURNING id_purchase
+                `;
+                const purchaseValues = [purchaseDate, user_id];
+                const { rows: purchaseRows } = await postgre.query(purchaseQuery, purchaseValues);
+                const purchaseId = purchaseRows[0].id_purchase;
+
+                // Inserção do item da compra
+                const itemQuery = `
+                    INSERT INTO items (user_id, product_id, purchase_id, quantity)
+                    VALUES ($1, $2, $3, $4)
+                    RETURNING *
+                `;
+                const itemValues = [user_id, product_id, purchaseId, quantity];
+                const { rows: itemRows } = await postgre.query(itemQuery, itemValues);
+
+                // Atualização do estoque
+                const updateStockQuery = `
+                    UPDATE products 
+                    SET stock = stock - $1 
+                    WHERE id_product = $2;
+                `;
+                await postgre.query(updateStockQuery, [quantity, product_id]);
+            }
 
             await postgre.query('COMMIT');
 
-            res.json({ msg: "Purchase completed successfully", data: itemRows[0] });
+            res.json({ msg: "Purchase completed successfully" });
 
         } catch (error) {
             await postgre.query('ROLLBACK');
-            console.error(error);
-            res.status(500).json({ msg: "Internal Server Error" });
-        }
-    },
-
-    getAllPurchasesByUser: async (req, res) => {
-        try {
-            const { user_id } = req.params;
-
-            const { rows: purchases } = await postgre.query("SELECT * FROM purchases WHERE user_id = $1", [user_id]);
-
-            res.json({ purchases });
-        } catch (error) {
             console.error(error);
             res.status(500).json({ msg: "Internal Server Error" });
         }
